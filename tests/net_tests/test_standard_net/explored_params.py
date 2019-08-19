4e04f289b3e3e256fd0e22670854062e4f1f9e15
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2019-08-19 15:23:20 +0200

    test standard network

---
+++ tests/net_tests/test_standard_net/explored_params.py
@@ -0,0 +1,24 @@
+from brian2.units import ms, mV, second, Hz
+from pypet import cartesian_product
+
+
+input_dict = {'crs_crrs_rec': [0],
+              'syn_scl_rec' : [0],
+              'syn_iscl_rec' : [0],
+              'synEE_rec': [0],
+              'synEI_rec': [0],
+              'syn_noise': [0],
+              'synee_activetraces_rec' : [0],
+              'synee_Apretraces_rec': [0],
+              'synee_Aposttraces_rec': [0],
+              'synee_a_nrecpoints': [1],
+              'synei_a_nrecpoints': [1],
+              'synEEdynrec': [1],
+              'synEIdynrec': [1],
+              'turnover_rec': [0]}
+
+
+name = 'test_standard_net'
+
+explore_dict = cartesian_product(input_dict)
+
