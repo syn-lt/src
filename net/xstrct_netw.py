6341be440999e568efeec002301303b343a3f78e
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2020-03-23 11:13:34 +0100

    fix indentation (non-critical)

--- net/xstrct_netw.py
+++ net/xstrct_netw.py
@@ -262,10 +262,10 @@ def run_net(tr):
 
     if tr.istdp_active and tr.synEI_rec:
 
-            synEI_pre_mod  = '''%s 
-                                %s''' %(synEI_pre_mod, mod.synEI_pre_rec)
-            synEI_post_mod = '''%s 
-                                %s''' %(synEI_post_mod, mod.synEI_post_rec)
+        synEI_pre_mod  = '''%s 
+                            %s''' %(synEI_pre_mod, mod.synEI_pre_rec)
+        synEI_post_mod = '''%s 
+                            %s''' %(synEI_post_mod, mod.synEI_post_rec)
             
     if tr.istdp_active:        
         SynEI = Synapses(target=GExc, source=GInh, model=synEI_mod,
