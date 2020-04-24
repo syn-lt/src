16b554c050807a889e635fdb32391260622b8189
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2020-04-24 15:20:59 +0200

    overview_winh standard analysis

--- run_analysis.sh
+++ run_analysis.sh
@@ -3,6 +3,7 @@
 rm -rf src/analysis
 cp -r ~/lab/netw-mod/analysis-dev/ src/analysis/
 
+python -m src.analysis.overview_winh
 #python -m code.analysis.overview
 #python -m src.analysis.t5_rasters
-python -m src.analysis.t5_peaks
+#python -m src.analysis.t5_peaks
